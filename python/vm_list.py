9f404a94de78eac3a4288df51fafda654d3755ad
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-05 22:31:48 +0530

    Update vm_list.py

--- python/vm_list.py
+++ python/vm_list.py
@@ -4,5 +4,6 @@ client = boto3.client('ec2')
 
 response = client.describe_instances()
 
-for x1 in response:
-  print(x1)
+for x1 in response["Reservations"]:
+  print(">>> ", x1["Instances"][0].get("Tags","NA"))
+  print(x1["Instances"][0]["State"])
