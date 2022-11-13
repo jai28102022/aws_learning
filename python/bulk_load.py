8f9d92ee871995f343fdfb7a97fd0e964b42d387
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-13 22:20:28 +0530

    Update bulk_load.py

--- python/bulk_load.py
+++ python/bulk_load.py
@@ -6,13 +6,16 @@ def load_csv():
     file = open(filename)
     data = csv.DictReader(file, delimiter=',')
     for x1 in data:
-        create_instance(x1.get("name"), x1.get("type"))
+        create_instance(x1.get("name"), x1.get("type"), x1.get("pkg"))
     file.close()
 
-def create_instance(instance_name, instance_type):
+def create_instance(instance_name, instance_type,pkg):
     machine_name = instance_name
     if machine_name:
        ec2 = boto3.resource('ec2')
+       userdata = """#!/bin/bash
+yum update
+apt install {0} -y"""
        imageid = 'ami-024c319d5d14b463e'
        instancetype = instance_type
        instance = ec2.create_instances(
@@ -20,7 +23,9 @@ def create_instance(instance_name, instance_type):
                    MinCount=1,
                    MaxCount=1,
                    InstanceType=instancetype,
-                   KeyName="jai" )
+                   KeyName="jai",
+                   UserData=userdata.format(pkg)
+                   )
        for x1 in instance:
           x1.create_tags(Tags=[{"Key": "Name", "Value": machine_name}])
        print(">>>", machine_name)
